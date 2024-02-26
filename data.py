372325dd698777d39ce0991e1cd6ec0961300bf0
James <james@jamesmcc.co.uk>
2024-02-26 15:46:54 +0000

    Added budgets and data targets

--- data.py
+++ data.py
@@ -1,24 +1,24 @@
+import pandas as pd
+import numpy as np
+
 
 # All Fusion Data
-def fusion_data():
-    with open("data/fusion.csv") as f:
-        return [x.strip().split(",") for x in f.readlines()[1:]]
+def get_data(building: str):
+    try:
+        # Get the building data
+        with open(f"data/{building}.csv") as f:
+            return [x.rstrip().split(",") for x in f.readlines()[1:]]
+    except FileNotFoundError as e:
+        return 404
 
-def fusion_budget():
-    return 1000
 
-
-# All Dorset House Data
-def dorset_house_data():
-    with open("data/dorset_house.csv") as f:
-        return [x.strip().split(",") for x in f.readlines()[1:]]
-
-# All Kimmeridge House Data
-def kimmeridge_data():
-    with open("data/kimmeridge.csv") as f:
-        return [x.strip().split(",") for x in f.readlines()[1:]]
-
-# All PGB Data
-def pgb_data():
-    with open("data/pgb.csv") as f:
-        return [x.strip().split(",") for x in f.readlines()[1:]]
+def budget(building: str):
+    try:
+        with open(f"data/{building}.csv") as f:
+            file_data = pd.read_csv(f)
+            mean_vals = {"energy": np.mean(file_data["incoming_energy"]).round(2),
+                         "gas": np.mean(file_data["incoming_gas"]).round(2),
+                         "water": np.mean(file_data["incoming_water"]).round(2)}
+            return mean_vals, sum(mean_vals.values())
+    except FileNotFoundError as e:
+        return 404
--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from flask import Flask, jsonify, make_response, render_template, request, url_f
 import data
 
 app = Flask(__name__,
-           static_folder='static')
+            static_folder='static')
 
-#app routes to different pages
+
+# app routes to different pages
 @app.route('/')
-def index(): # Main Page
-  return render_template('index.html')
+def index():  # Main Page
+    return render_template('index.html')
 
 
 # |== Config Routes ==|
@@ -24,39 +25,21 @@ def get_prices():
         return json.load(f)
 
 
+# |== FUSION DATA ==|
 
-# |== FUSION DATA ==| 
-
-@app.route("/fusion")
-def fusion_all_data():
+@app.route("/data/<building>")
+def all_data(building):
     # Get All Data about Fusion Building
-    return data.fusion_data()
+    return data.get_data(building)
 
 
-@app.route("/fusion/budget")
-def fusion_budget():
+@app.route("/budget/<building>")
+def budget(building):
     # Get Budget Data about Fusion Building
-    return {"budget": data.fusion_budget()}
-
-
-# |== DORSET HOUSE DATA ==|
-
-@app.route("/dorset_house")
-def dorset_house_all_data():
-    # Get all data about Dorset House
-    return data.dorset_house_data()
-
-@app.route("/kimmeridge")
-def kimmeridge_all_data():
-    # Get all data about Kimmeridge House
-    return data.kimmeridge_data()
-
-@app.route("/pgb")
-def pgb_all_data():
-    # Get all data about PGB
-    return data.pgb_data()
+    budget_data = data.budget(building)
+    return {"budget": budget_data[0], "total": budget_data[1]}
 
 
 # Main Loop
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
