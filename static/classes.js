7cfc39ace9261737dd4ad58b83836a0dcc2fe279
Andrew Carter <68480855+AndrewCarter04@users.noreply.github.com>
2024-02-26 15:31:29 +0000

    Continuing adding balls to buildings

--- static/classes.js
+++ static/classes.js
@@ -19,19 +19,31 @@ class Building {
   getSize() { return this.size; }
   draw() { return square(this.x, this.y, this.size); }
   getBalls() { return this.balls; }
+  addBall(ball) { this.balls.push(ball); }
 }
 
 //Ball
 class Ball {
+  size = 10;
   constructor(type, building) {
     this.type = type;
-    this.size = 10;
     this.building = building;
-    building.getBalls().push(this);
   }
   getType() { return this.type; }
   getSize() { return this.size; }
   getBuilding() { return this.building; }
-  draw(x, y) { return ellipse(x, y, this.size, this.size); }
+  draw(x, y) { 
+    if (this.type == 'W') {
+      // blue
+      fill(0,0,255);
+    } else if (this.type == 'E') {
+      // yellow
+      fill(255,255,0);
+    } else if (this.type == 'G') {
+      // purple
+      fill(255,0,255);
+    }
+    return ellipse(x, y, size, size); 
+  }
   kill() { /* Remove from building */ }
 }
--- static/script.js
+++ static/script.js
@@ -1,17 +1,18 @@
 //Setting Building Details
-const waterCost = 370;
-const electricCost = 45;
-
-
-
-let fusion = new Building('F', 20, 20, 1000);
-let pooleGateway = new Building('PG', 140, 20, 1000);
-let dorsetHouse = new Building('D', 260, 20, 1000);
-let kimmeridge = new Building('K', 380, 20, 1000);
+const waterCost = 370; // type = 'W'
+const electricCost = 45; // type = 'E'
+const gasCost = 10.7; // type = 'G'
 
 function setup() {
   createCanvas(windowWidth-10, windowHeight*0.9);
+
 }
+
+//Makings Buildings
+let fusion = new Building('F', 20, 100, 1000);
+let pooleGateway = new Building('PG', 140, 100, 1000);
+let dorsetHouse = new Building('D', 260, 100, 1000);
+let kimmeridge = new Building('K', 380, 100, 1000);
 
 function draw() {
   background(220);
@@ -30,17 +31,31 @@ function draw() {
 function mouseClicked() {
   //Check if mouse is over a building
   if (mouseX > fusion.getX() && mouseX < fusion.getX() + fusion.getSize() && mouseY > fusion.getY() && mouseY < fusion.getY() + fusion.getSize()) {
+    //Fusion
     console.log("Fusion")
+  } else if (mouseX > pooleGateway.getX() && mouseX < pooleGateway.getX() + pooleGateway.getSize() && mouseY > pooleGateway.getY() && mouseY < pooleGateway.getY() + pooleGateway.getSize()) {
+    //Poole Gateway
+    console.log("pooleGateway")
+  } else if (mouseX > kimmeridge.getX() && mouseX < kimmeridge.getX() + kimmeridge.getSize() && mouseY > kimmeridge .getY() && mouseY < kimmeridge.getY() + kimmeridge.getSize()) {
+    //Kimmeridge
+    console.log("kimmeridge")
+  } else if (mouseX > dorsetHouse.getX() && mouseX < dorsetHouse.getX() + dorsetHouse.getSize() && mouseY > dorsetHouse.getY() && mouseY < dorsetHouse.getY() + dorsetHouse.getSize()) {
+    //Kimmeridge
+    console.log("kimmeridge")
   }
 }
 
-function addBall(building) {
+function addBall(type, building) {
   //Adds a ball to the building
   if (building.getBudget() < building.getBalls().length) {
     // enough capacity
-    let ball = new Ball('', building);
+    let ball = new Ball(type, building);
+    building.addBall(ball);
+    ball.draw(100, 100);
   } else {
     // not enough capacity
-    let ball = new Ball('', null);
+    let ball = new Ball(type, null);
   }
 }
+
+addBall('W', fusion);
